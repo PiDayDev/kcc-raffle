97a2053e8c5ce368e13ff24b859d40b4492e9507
Salvi <salvi@intre.it>
2018-06-05 14:54:58 +0200

    Project setup

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+group = "it.intre.kcc"
+version = "1.0-SNAPSHOT"
+
+plugins {
+    application
+    kotlin("jvm") version "1.2.31"
+}
+
+repositories {
+    mavenCentral()
+    jcenter()
+}
+
+dependencies {
+    compile(kotlin("stdlib"))
+    testCompile("junit", "junit", "4.12")
+}
+
+application {
+    mainClassName = "samples.HelloWorldKt"
+}
